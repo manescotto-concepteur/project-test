d1b614e3b7df7cb59eb45b3aadd387dcde7061a4
manescotto-concepteur <manescotto.concepteur@gmail.com>
2014-10-08 09:21:38 +0200

    Update ClasseBidon.java

--- TEST/src/ClasseBidon.java
+++ TEST/src/ClasseBidon.java
@@ -4,5 +4,8 @@ public class ClasseBidon {
 	private int nb;
 	private String ignore;
 	private int i;
+	// Ces variables ont etes creees apres le commit de la deuxieme branche
+	private int j;
+	private int k;
 
 }
